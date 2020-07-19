11a389a310e3ab7d6fcc65e3f50399fa620b784f
kinsprite <qxqs11@126.com>
2020-07-19 12:03:17 +0800

    use WebView to open URL

--- app/src/main/java/cn/qinzhiqiang/webview1/MainActivity.java
+++ app/src/main/java/cn/qinzhiqiang/webview1/MainActivity.java
@@ -3,12 +3,61 @@ package cn.qinzhiqiang.webview1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    private long exitTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWebView();
+    }
+
+    private void initWebView() {
+        webView = (WebView)findViewById(R.id.blog_webview);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDefaultFontSize(16);
+
+        //设置缓存模式
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        // 开启DOM storage API 功能
+        settings.setDomStorageEnabled(true);
+
+        webView.loadUrl("https://micro.qinzhiqiang.cn");
+
+        webView.setWebViewClient(new WebViewClient() {
+            //在webview里打开新链接，否则会使用系统中浏览器打开新链接
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(getApplicationContext(), "再按一次退出程序",
+                        Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+        }
     }
 }
